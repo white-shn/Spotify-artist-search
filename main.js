7da1496eb0301d315deba60e72bb70709f5e70a6
Nishant <nishantgolhani085@gmail.com>
2026-06-17 11:34:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ async function searchArtist(artist) {
     console.log(data);
 
     const artistData = data.artists.items[0];
+    console.log("artistData:", artistData);
+    console.log("followers:", artistData.followers);
 
     document.querySelector(".artist-name").innerHTML =
         artistData.name;
